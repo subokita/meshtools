f5c69b2916675eaf374dad6091d9d35fc24e3aca
Tom Fogal <tfogal@alumni.unh.edu>
2012-11-20 19:00:56 +0100

    Take color image ('texture') as command line param.
    Clean up option handling just a tad, too.

--- depth.c
+++ depth.c
@@ -10,14 +10,31 @@
 #include "wrpng.h"
 
 int main(int argc, char* argv[]) {
-  if(argc != 3) {
-    fprintf(stderr, "Usage: %s depthimage obj-file\n", argv[0]);
+  if(argc != 4) {
+    fprintf(stderr, "Usage: %s depthimage colorimage obj-file\n", argv[0]);
     exit(EXIT_FAILURE);
   }
+  const char* depthfile = argv[1];
+  const char* colorfile = argv[2];
+  const char* objfile = argv[3];
+
   uint8_t* img;
   uint32_t width, height;
-  readpng(argv[1], &img, &width, &height);
-  write_obj8("frame", "cdata-01155.png", img, width, height, 0.1f);
+
+  readpng(depthfile, &img, &width, &height);
+
+  {
+    /* generate the basename for the OBJ file by trying to find a "."
+    and hacking off everything there and later. */
+    char* base_obj = calloc(strlen(objfile)+1, sizeof(char));
+    strcpy(base_obj, objfile);
+    char* dot = strrchr(base_obj, '.');
+    if(dot) { *dot = '\0'; }
+
+    write_obj8(base_obj, colorfile, img, width, height, 0.1f);
+    free(base_obj);
+  }
+
   free(img);
 
   return EXIT_SUCCESS;
